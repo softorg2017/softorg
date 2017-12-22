298a121e8e8fa7f94f53f82ea37d7ddc765e5c63
longyun <longyun.cui88@gmail.com>
2017-12-22 15:03:01 +0800

    survey

--- resources/views/admin/survey/edit.blade.php
+++ resources/views/admin/survey/edit.blade.php
@@ -63,18 +63,18 @@
                 <div class="form-group" style="display: none;">
                     <label class="control-label col-md-2">内容详情</label>
                     <div class="col-md-8 ">
-                        {{--<div>--}}
-                            {{--@include('UEditor::head')--}}
-                            {{--<!-- 加载编辑器的容器 -->--}}
-                            {{--<script id="container" name="content" type="text/plain">{!! $data->content or '' !!}</script>--}}
-                            {{--<!-- 实例化编辑器 -->--}}
-                            {{--<script type="text/javascript">--}}
-                                {{--var ue = UE.getEditor('container');--}}
-                                {{--ue.ready(function() {--}}
-                                    {{--ue.execCommand('serverparam', '_token', '{{ csrf_token() }}');//此处为支持laravel5 csrf ,根据实际情况修改,目的就是设置 _token 值.--}}
-                                {{--});--}}
-                            {{--</script>--}}
-                        {{--</div>--}}
+                        <div>
+                            @include('UEditor::head')
+                            <!-- 加载编辑器的容器 -->
+                            <script id="container" name="content" type="text/plain">{!! $data->content or '' !!}</script>
+                            <!-- 实例化编辑器 -->
+                            <script type="text/javascript">
+                                var ue = UE.getEditor('container');
+                                ue.ready(function() {
+                                    ue.execCommand('serverparam', '_token', '{{ csrf_token() }}');//此处为支持laravel5 csrf ,根据实际情况修改,目的就是设置 _token 值.
+                                });
+                            </script>
+                        </div>
                         <div><input type="text" class="form-control" name="content" placeholder="描述" value="{{$data->content or ''}}"></div>
                     </div>
                 </div>
