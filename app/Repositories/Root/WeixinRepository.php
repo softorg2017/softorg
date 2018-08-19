ee27f1c0de22c26c63f21fc9259af1250cc8aaac
longyun <longyun.cui88@gmail.com>
2018-08-20 02:58:01 +0800

    weixin

--- app/Repositories/Root/WeixinRepository.php
+++ app/Repositories/Root/WeixinRepository.php
@@ -116,6 +116,9 @@ class WeixinRepository {
 
         // 获取一般用户信息
         // $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token={$access_token}&openid={$openid}&lang=zh_CN";
+
+        $info = $this->getInfo($openid);
+        var_dump($info);
     }
 
     //
