a56f9869bd3c92e9c461d3a0f8da32cd91796d6b
Lonyun Cui <longyun.cui88@gmail.com>
2021-04-30 21:43:42 +0800

    weixin.share

--- resources/views/www/frontend/entrance/user.blade.php
+++ resources/views/www/frontend/entrance/user.blade.php
@@ -14,7 +14,7 @@
 @section('meta_keywords')@endsection
 
 
-@section('wx_share_title') @if(!empty($data->ture_name)){{ $data->ture_name or '如未科技' }}@else{{ $data->username or '如未科技' }}@endif @endsection
+@section('wx_share_title') @if(!empty($data->true_name)){{ $data->true_name or '如未科技' }}@else{{ $data->true_name or '如未科技' }}@endif @endsection
 @section('wx_share_desc') @if(!empty($data->company)){{ $data->company or '' }}@endif / @if(!empty($data->position)){{ $data->position or '' }}@endif @endsection
 @section('wx_share_imgUrl'){{ url(env('DOMAIN_CDN').'/'.$data->portrait_img) }}@endsection
 
